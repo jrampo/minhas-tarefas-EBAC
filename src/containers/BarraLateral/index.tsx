e24d207821723b8c35654e4b8ae6e6b66c004df4
Joao Rampo <joao.rampoo@gmail.com>
2024-08-13 17:55:24 -0300

    eslint bugging + card create

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,5 +1,21 @@
+import FiltroCard from '../../components/FiltroCard'
+
 const BarraLateral = () => {
-  return <aside>barra lateral</aside>
+  return (
+    <aside>
+      <div>
+        <input type="text" placeholder="Buscar" />
+        <div>
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+        </div>
+      </div>
+    </aside>
+  )
 }
 
 export default BarraLateral
